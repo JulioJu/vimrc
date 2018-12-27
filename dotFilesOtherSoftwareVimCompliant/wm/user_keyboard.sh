1ee51e2600027033a80224964585a88ff3993a4a
JulioJu <juanes0890@gmail.com>
2018-12-27 17:18:04 +0100

    Add lafayette layout 0.6.1 (not offically published for non-root user use)
    `$ xkbcomp -w9 ~/.vim/dotFilesOtherSoftwareVimCompliant/lafayette_linux_v0.6.xkb $DISPLAY`

--- dotFilesOtherSoftwareVimCompliant/wm/user_keyboard.sh
+++ dotFilesOtherSoftwareVimCompliant/wm/user_keyboard.sh
@@ -5,4 +5,5 @@ USERID=julioprayer
 export DISPLAY=":0"
 export XAUTHORITY="/home/$USERID/.Xauthority"
 xinput float "AT Translated Set 2 keyboard"
-xkbcomp -w9 /opt/lafayette_linux_v0.5.xkb $DISPLAY
+xkbcomp -w9 /opt/lafayette_linux_v0.6.xkb $DISPLAY
+xmodmap -e "keycode 102 = Shift_L"
